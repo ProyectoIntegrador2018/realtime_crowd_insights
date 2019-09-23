4c512b7edbcced5d14f61023fbb4fc40f18b17dd
Miguel Bazan <miguelb1995@icloud.com>
2019-09-23 15:58:42 -0500

    Create project

---
+++ realtime_crowd_insights/realtime_crowd_insights/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  realtime_crowd_insights
+//
+//  Created by Miguel Bazán on 9/23/19.
+//  Copyright © 2019 Miguel Bazán. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
